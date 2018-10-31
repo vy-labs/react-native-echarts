d389f2c949fc34a2874696ec88faa17410eb477e
jssridhar <josh.sridhar1@gmail.com>
2018-10-31 17:55:36 +0530

    theme support

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -4,10 +4,13 @@ import toString from '../../util/toString';
 export default function renderChart(props) {
   const height = `${props.height || 400}px`;
   const width = props.width ? `${props.width}px` : 'auto';
+  const theme = props.theme ? toString(props.theme) || '';
+
   return `
     document.getElementById('main').style.height = "${height}";
     document.getElementById('main').style.width = "${width}";
-    var myChart = echarts.init(document.getElementById('main', ${props.theme || {}}));
+    echarts.registerTheme('synaptic', ${theme});
+    var myChart = echarts.init(document.getElementById('main', 'synaptic');
     myChart.setOption(${toString(props.option)});
     window.document.addEventListener('message', function(e) {
       var option = JSON.parse(e.data);
