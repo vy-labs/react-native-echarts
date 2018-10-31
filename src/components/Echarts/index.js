da4221b01750f111523d17f737ab80e458861807
jssridhar <josh.sridhar1@gmail.com>
2018-10-31 17:56:07 +0530

    revert changes

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { WebView, View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
-// import echarts from './echarts.min';
+import echarts from './echarts.min';
 
 export default class App extends Component {
 
